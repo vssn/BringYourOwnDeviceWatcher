f71a3f3cac3b786514a7900548d6148953a20176
Edmund Goller <edmund.goller@artdesk.de>
2019-02-24 14:32:21 +0100

    First commit

---
+++ BringYourOwnDeviceWatcher/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BringYourOwnDeviceWatcher.Models;
+using System.Xml.Serialization;
+using System.Data;
+
+namespace BringYourOwnDeviceWatcher.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Index([FromBody] NmapRun devices)
+        {
+            return devices.Verbose.Level; //View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BringYourOwnDeviceWatcher/Models/NmapRun.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BringYourOwnDeviceWatcher.Models
+{
+    [XmlRoot("nmaprun")]
+    public class NmapRun
+    {
+        [XmlElement("verbose")]
+        public Verbose Verbose { get; set; }
+
+        [XmlElement("host")]
+        public List<Host> Host { get; set; }
+    }
+
+    public class Verbose
+    {
+        [XmlAttribute("level")]
+        public string Level { get; set; }
+
+    }
+
+    public class Host
+    {
+        [XmlElement("status")]
+        public Status Status { get; set; }
+
+        [XmlArray("hostnames")]
+        [XmlArrayItem("hostname")]
+        public List<Hostname> Hostname { get; set; }
+
+        [XmlElement("address")]
+        public List<Address> Address { get; set; }
+    }
+
+    public class Address
+    {
+        [XmlAttribute]
+        public string addr { get; set; }
+
+        [XmlAttribute]
+        public string addrtype { get; set; }
+    }
+
+    public class Status
+    {
+        [XmlAttribute]
+        public string state { get; set; }
+    }
+
+    public class Hostname
+    {
+        [XmlAttribute]
+        public string name { get; set; }
+
+        [XmlAttribute]
+        public string type { get; set; }
+    }
+}
